02efa4044796dd92fb0f1d6b96c1d4d6c836967b
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-11 15:38:48 +0100

    define routes

--- laravel-manual-auth/routes/web.php
+++ laravel-manual-auth/routes/web.php
@@ -2,6 +2,65 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+// Guest Routes
+Route::middleware('guest')->group(function(){
+
+// Login
+Route::get('/login' , [LoginController::class , 'create'])->name('login');
+Route::post('/login' , [LoginController::class ,'store']);
+
+// Register
+Route::get('/register' , [RegisterController::class , 'create'])->name('register');
+Route::post('/register' , [registerController::class , 'store']);
+
+// Password reset
+Route::get('/forget-password' , [PasswordResetController::class , 'create'])
+->name('password.request');
+Route::post('/forgot-password' , [PasswordResetController::class , 'store'])
+->name('password.email');
+Route::get('/reset-password/{token}' , [PasswordResetController::class , 'showResetForm'])
+->name('password.reset');
+Route::post('/reset-password' , [PasswordResetController::class , 'update'])
+->name('password.update');
+
 });
+
+
+
+// Authenticated Routes
+Route::middleware('auth')->group(function(){
+    //dashboard
+    Route::get('/dashboard' ,  DashboardController::class)->name('dashboard');
+
+    // logout
+    Route::post('/logout' , [LoginController::class , 'logout'])->name('logout');
+
+    // email verification
+    Route::controller(EmailverificationController::class)->group(function(){
+        Route::get('/email/verify' , 'notice')->name('verfiication.notice');
+        Route::get('/email/verify{id}/{hash}' ,'verify')
+        ->middleware('signed')
+        ->name('verification.verify');
+        Route::post(':email/verification-notification' ,'send')
+        ->middleware('throttle:6,1')
+        ->name('verification.send');        
+    });
+
+    // Profiel (requires verified email    )
+    Route::middleware('verified')->group(function(){
+        Route::get('/profile' , [ProfileController::class , 'edit'])->name('profile.edit');
+        Route::put('/profile' , [ProfileController::class , 'update'] )->name('profile.update');
+        Route::put('/profile/password' , [ProfileController::class, 'updatePassword'])
+        ->name('profile.password.update');
+        Route::delete('/profile/photo' , [profileController::class , 'destroyPhoto'])
+        ->name('profile.photo.destroy');
+    });
+});
+
+
+// home page 
+Route::get('/' , function(){
+    return view('welcome');
+})->name('home');
